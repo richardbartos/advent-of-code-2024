528a779a1eccfd30aefbb155ef2ee56c2a8c00ed
richardbartos <ricci21@gmail.com>
2024-12-02 20:37:36 +0100

    Day 01 Part 2 - Solution

--- day01.py
+++ day01.py
@@ -32,6 +32,34 @@ def find_distances(data):
     return distance
 
 
+def calculate_similarity(data):
+    """
+    >>> calculate_similarity(["3   4",
+    ... "4   3",
+    ... "2   5",
+    ... "1   3",
+    ... "3   9",
+    ... "3   3"])
+    31
+    """
+
+    similarity = 0
+    edited_data = data
+    first_list = []
+    second_list = []
+
+    for item in edited_data:
+        first_list.append(item.split()[0])
+        second_list.append(item.split()[1])
+
+    first_list.sort()
+    second_list.sort()
+
+    for i in range(len(first_list)):
+        similarity += int(first_list[i])*second_list.count(first_list[i])
+
+    return similarity
+
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), 'day01.txt')) as fh:
         for line in fh.readlines():
@@ -39,3 +67,4 @@ if __name__ == "__main__":
             data_input.append(line)
 
     print("Total distances: " + str(find_distances(data_input)))
+    print("Similarity score: " + str(calculate_similarity(data_input)))
